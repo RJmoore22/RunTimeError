e5fa9304f4ef1cac2ac20c907b4d2a43861fe64a
RJmoore22 <rjmoore2@oakland.edu>
2023-03-23 09:52:36 -0400

    Add files via upload

---
+++ March21UpdatePart2.py
@@ -0,0 +1,280 @@
+import tkinter
+import time
+import random
+import tracemalloc
+PYTHONUNBUFFERED="yes"
+
+TK_SILENCE_DEPRECATION=1
+
+Window_Width=1600
+Window_Height=800
+line_width = Window_Width - 20
+rect_min_movement = 5
+Refresh_Sec = .000001
+Refresh_Sec2 =.000001
+rectx1 = 10
+rectx2 = rectx1 + 10
+basey = Window_Height - 10
+
+#creating an empty array list 
+yval = []
+despos = []
+names = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten' ]
+errorCounter = 0
+objects = []
+
+
+
+
+colnum = 222
+
+
+
+
+colwidth = round(((Window_Width - 18) / colnum)-2)
+colbetween = colwidth + 2
+
+#timex = 0 for fast or 1 for slow 
+
+def fast():
+    global timex
+    timex = 0
+    bubbleSort(yval)
+
+def Bubble():
+    global timex
+    timex = 1
+    bubbleSort(yval)
+
+color2 = "#386e6b"
+def animate_rect():
+    
+    objects.clear()
+    yval.clear()
+    canvas.delete('all')
+    despos.clear()
+    canvas.create_line(8,basey,Window_Width - 10,basey,fill = "white") 
+    #implemented a incremental counter, while Inc_bar is less than Max_bar columns will continue to generate
+    Inc_bar = 0
+    # Change Max_bar to change the max number of columns being made
+    Max_bar = 100
+    x1pos = 10
+    xstart = 10
+    
+    for i in range(0,colnum):
+        delay = 1
+        color = "#%03x" % random.randint(0, 0xFFF)
+        color2 = "#386e6b"
+        #x2pos= x1pos + colwidth
+        y2pos = random.randint(10,basey - 10)
+        rect = canvas.create_rectangle(xstart-(colbetween * delay),basey,xstart-(colbetween * delay)+colwidth, y2pos, fill = color2, outline = "")
+        objects.append(rect)
+        
+        x1pos += colbetween
+        val = basey - y2pos
+        yval.append(val)
+        
+        #print (len(yval), len(despos))
+        if i == 0:
+            Inc_bar = 10
+        else:
+            Inc_bar += colbetween
+        while True:
+            #canvas.config(objects[i], fill = 'red')
+            canvas.move(rect,colbetween,0)
+            #canvas.update()
+            #time.sleep(Refresh_Sec)
+            rect_pos = canvas.coords(rect)
+            # unpack array to variables
+            x1,y1,x2,y2 = rect_pos
+            despos.append(x1)
+            z = x1 
+            if z == Inc_bar:
+                xstart += colbetween 
+                break
+    lowest = yval.index(min(yval))
+    highest = yval.index(max(yval))
+    canvas.itemconfig(objects[lowest],fill = 'red')
+    canvas.itemconfig(objects[highest],fill = 'red')
+    canvas.update()
+    #start = yval.index(min(yval))
+    #end = yval.index(max(yval))
+
+     
+def bubbleSort(yval): 
+    tracemalloc.start()
+    tic = time.perf_counter()   
+    n = len(yval)  
+    
+    # Traverse through all array elements
+    for i in range(0,n-1):
+        if timex == 1:
+            canvas.update()
+        # Last i elements are already in place
+        for j in range(n-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if yval[j] > yval[j + 1]:
+                yval[j], yval[j+1] = yval[j+1], yval[j]
+                
+                    
+                canvas.move(objects[j],colbetween, 0)
+                canvas.move(objects[j+1],-colbetween, 0)
+                objects[j], objects[j+1] = objects[j+1], objects[j]
+                    
+
+    toc = time.perf_counter()
+    canvas.create_text(Window_Width / 2, 40, text=f"Traced Memory (Current, Peak): {tracemalloc.get_traced_memory()} bytes ", fill = "black")
+    canvas.create_text(Window_Width / 2, 20, text=f"{toc-tic:0.3f} seconds ", fill = "black")
+    tracemalloc.stop()
+
+def insertionSort(yval):
+    tracemalloc.start()
+    st = time.perf_counter()
+    n = len(yval)
+    for i in range(1, n):
+        k = yval[i]
+        j = i - 1
+        canvas.update()
+        while j >= 0 and k < yval[j]:
+            yval[j + 1] = yval[j]
+            canvas.move(objects[j],colbetween, 0)
+            canvas.move(objects[j+1],-colbetween, 0)
+            objects[j], objects[j+1] = objects[j+1], objects[j]
+            j -= 1
+        yval[j + 1] = k
+        
+    #print(yval)
+    et = time.perf_counter()
+    canvas.create_text(Window_Width / 2, 40, text=f"Traced Memory (Current, Peak): {tracemalloc.get_traced_memory()} bytes ", fill = "black")
+    canvas.create_text(Window_Width / 2, 20, text=f"{et-st:0.3f} seconds ", fill = "black")
+    tracemalloc.stop()
+
+def mergeSort(yval,despos,objects):
+    print(len(yval))
+    print(len(despos))
+    if len(yval) > 1:
+         # Finding the mid of the array
+        mid = len(yval)//2
+        # Dividing the array elements
+        L = yval[:mid]
+        L2 = despos[:mid]
+        L3 = objects[:mid]
+        # into 2 halves
+        R = yval[mid:]
+        R2 = despos[mid:]
+        R3 = objects[mid:]
+        # Sorting the first half
+        mergeSort(L,L2,L3)
+        # Sorting the second half
+        mergeSort(R,R2,R3)
+        i = j = k = 0
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] >= R[j]:     
+                yval[k] = L[i]
+                despos[k] = L2[i]
+                objects[k] = L3[i]
+                canvas.delete(objects[k])
+                objects[k] = canvas.create_rectangle(despos[i],basey,despos[k] + colwidth, yval[k], fill = color2, outline = "")
+                canvas.update()
+                i += 1
+            else:
+                yval[k] = R[j]
+                despos[k] = R2[j]
+                objects[k] = R3[j]
+                canvas.delete(objects[k])
+                objects[k] = canvas.create_rectangle(despos[j],basey,despos[k] + colwidth, yval[k], fill = color2, outline = "")
+                canvas.update()
+                j += 1
+            k += 1
+        # Checking if any element was left
+        while i < len(L):
+            yval[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            yval[k] = R[j]
+            j += 1
+            k += 1
+
+
+
+def partition(yval, start, end):
+
+
+    pivot = yval[end]
+    canvas.itemconfig(objects[end], fill = "green")
+    canvas.update()
+    i = start -1
+
+    for j in range(start, end):
+        if yval[j] <= pivot:
+
+            i = i + 1
+            yval[i], yval[j] = yval[j], yval[i]
+            
+            canvas.move(objects[i],colbetween * (j - i), 0)
+            canvas.move(objects[j],-colbetween * (j - i), 0)
+            objects[i], objects[j] = objects[j], objects[i]
+            canvas.update()
+
+    canvas.itemconfig(objects[end], fill = color2)
+    canvas.update()
+    yval[i + 1], yval[end] = yval[end], yval[i + 1]
+    canvas.move(objects[i+1],colbetween * (end - (i+1)), 0)
+    canvas.move(objects[end],-colbetween * (end - (i+1)), 0)
+    objects[i+1], objects[end] = objects[end], objects[i+1]
+    canvas.update()
+    return i + 1
+    
+    
+
+def quickSort(yval, start, end):
+    
+    if start < end:
+
+        pi = partition(yval, start, end)
+
+        quickSort(yval, start, pi - 1)
+
+        quickSort(yval, pi + 1, end)
+
+    
+Window = tkinter.Tk()
+Window.title("tk")
+#Window.geometry(f'{Window_Width}x{Window_Height}')
+Window.lift()
+Window.attributes("-topmost", True)
+
+
+
+    
+UI_frame = tkinter.Frame(Window, background = 'white', border = 30)
+UI_frame.grid(row=0, column=0, padx = 0, pady = 0)
+
+#animation canvas config
+canvas = tkinter.Canvas(UI_frame)
+canvas.configure(bg="white",width=Window_Width, height=Window_Height)
+canvas.pack(fill="both", expand=True)
+
+
+btn = tkinter.Button(UI_frame, text = 'Shuffle',bd = 5,command = lambda: [animate_rect(),animate_rect(),animate_rect(),animate_rect(),animate_rect(),animate_rect(),animate_rect(), animate_rect(), animate_rect(), animate_rect(),animate_rect(), animate_rect(), animate_rect(), animate_rect()])
+btn.pack(side = 'left')
+
+btn = tkinter.Button(UI_frame, text = 'Bubble',bd = 5,command = lambda: Bubble())
+btn.pack(side = 'right')
+
+btn = tkinter.Button(UI_frame, text = 'Insertion',bd = 5,command = lambda: insertionSort(yval))
+btn.pack(side = 'right')
+
+btn = tkinter.Button(UI_frame, text = 'Merge',bd = 5,command = lambda: mergeSort(yval,despos,objects))
+btn.pack(side = 'right')
+
+btn = tkinter.Button(UI_frame, text = 'Quick',bd = 5,command = lambda: quickSort(yval, 0, len(yval) - 1))
+btn.pack(side = 'right')
+
+Window.mainloop()
